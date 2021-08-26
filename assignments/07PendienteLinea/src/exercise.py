a4b643fe63813602f5c45e907b22aa58fe7a5ad6
edan11v <a014119126@itesm.mx>
2021-08-26 10:57:07 -0500

    Ejercicio "Pendiente" resuelto

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    pendiente = (y2-y1)/(x2-x1)
+    print(f'Pendiente: {pendiente}')
 
 
 if __name__ == '__main__':

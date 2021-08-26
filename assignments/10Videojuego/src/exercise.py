0a2586fce8865c1dfa707828d686b232ae0b086e
edan11v <a014119126@itesm.mx>
2021-08-26 14:34:21 -0500

    "10Videojuego"

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+    juegos_nuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    juegos_usados = int(input("Dame la cantidad de juegos usados: "))
+    costo_total = (juegos_nuevos*1000) + (juegos_usados*350)
+    print(f'El total de la compra es: {costo_total}')
 
 
 if __name__ == '__main__':

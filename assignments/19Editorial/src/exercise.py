2a3d7e6690405861aff57dc28709402f94f8b03c
edan11v <a014119126@itesm.mx>
2021-08-26 17:44:55 -0500

    "19Editorial"

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+    from math import ceil
+    num_palabras = int(input('Dame el número de palabras: '))
+    if num_palabras<=475:
+        costo = 54
+        print(f'El costo de la publicación es: {costo}')
+    else:
+        paginas = float(num_palabras)/475
+        total_pag = int(ceil(paginas))
+        costo = float(total_pag*54)
+        print(f'El costo de la publicación es: {costo}')
 
 
 if __name__ == '__main__':

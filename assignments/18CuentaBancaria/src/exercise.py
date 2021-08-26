eb498a845ac9f7ff9bc177060959ca0fa7041153
edan11v <a014119126@itesm.mx>
2021-08-26 14:52:18 -0500

    "18CuentaBancaria"

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,7 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
-
+    saldo_mes_anterior = float(input('Dame el saldo del mes anterior: '))
+    ingresos = float(input('Dame los ingresos: '))
+    egresos = float(input('Dame los egresos: '))
+    num_cheques = float(input('Dame el número de cheques: '))
+    balance = ( (num_cheques*13) + saldo_mes_anterior + ingresos - egresos ) 
+    interes_banco = (balance*7.5)/100
+    final=balance-interes_banco
+    print(f'El saldo final de la cuenta es: {final}')
 
 if __name__ == '__main__':
     main()

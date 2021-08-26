73fe3983bb701e7c6ad12eecd03cdee00225b571
edan11v <a014119126@itesm.mx>
2021-08-26 14:25:29 -0500

    "09Telefono" realizado

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
+    mensajes = float(input("Dame el número de mensajes: "))
+    megas = float(input('Dame el número de megas: '))
+    minutos = float(input('Dame el número de minutos: '))
+    costo_mensual = ((mensajes+megas+minutos)*0.80)
+    print(f'El costo mensual es: {costo_mensual}')
 
 
 if __name__ == '__main__':

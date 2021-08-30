2edec95be9059be60addbb990f4537a5642e0bab
edan11v <a014119126@itesm.mx>
2021-08-30 10:13:11 -0500

    "19Editorial"

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -4,7 +4,7 @@ def main():
     from math import ceil
     num_palabras = int(input('Dame el número de palabras: '))
     if num_palabras<=475:
-        costo = 54
+        costo = 54.0
         print(f'El costo de la publicación es: {costo}')
     else:
         paginas = float(num_palabras)/475
